91cb6de4d027fafac8369a12c5157b80eb405446
Marek Rost <marek.rost@gmail.com>
2017-12-05 08:25:51 +0100

    site header and footer moved

--- index.php
+++ index.php
@@ -6,42 +6,35 @@
 <body>
   <?php Theme::plugins('siteBodyBegin') ?>
 
-  <div id="fh5co-main">
-    <div id="fh5co-content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-3">
-            <div id="fh5co-main">
-              <div class="fh5co-intro text-center">
-                <h1 class="intro-lead"><a class="home-title" href="<?php echo $Site->url() ?>"><?php echo $Site->title() ?></a></h1>
-                <p class=""><?php echo $Site->description() ?></p>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3">
+        <header id="site-header">
+          <h1 class="intro-lead"><a class="home-title" href="<?php echo $Site->url(); ?>"><?php echo $Site->title(); ?></a></h1>
+          <p class=""><?php echo $Site->description(); ?></p>
+        </header>
+        <?php include(THEME_DIR_PHP.'sidebar.php'); ?>
+      </div>
+      <div class="col-md-9 col-lg-8">
+        <?php
+          if ($WHERE_AM_I=='page') {
+            include(THEME_DIR_PHP.'page.php');
+          } else {
+            include(THEME_DIR_PHP.'home.php');
+          }
+        ?>
+        <footer id="site-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-10 col-md-offset-1 text-center">
+                <p><?php echo $Site->footer(); ?><br>Powered by <a href="https://www.bludit.com" target="_blank">BLUDIT</a></p>
               </div>
             </div>
-            <?php include(THEME_DIR_PHP.'sidebar.php'); ?>
           </div>
-          <div class="col-md-9 col-lg-8">
-            <?php
-              if ($WHERE_AM_I=='page') {
-                include(THEME_DIR_PHP.'page.php');
-              } else {
-                include(THEME_DIR_PHP.'home.php');
-              }
-            ?>
-          </div>
-        </div>
+        </footer>
       </div>
     </div>
   </div>
-
-  <footer id="fh5co-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-10 col-md-offset-1 text-center">
-          <p><?php echo $Site->footer(); ?><br>Powered by <a href="https://www.bludit.com" target="_blank">BLUDIT</a></p>
-        </div>
-      </div>
-    </div>
-  </footer>
 
   <?php Theme::plugins('siteBodyEnd'); ?>
 </body>
