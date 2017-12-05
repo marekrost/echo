bb8b89bc6655c424ce73988fc49ac49771ae0a03
Marek Rost <marek.rost@gmail.com>
2017-12-05 07:19:23 +0100

    moved blog name over to side, applied some of the original css

--- index.php
+++ index.php
@@ -1,60 +1,48 @@
 <!DOCTYPE html>
 <html class="no-js" lang="<?php echo $Site->language() ?>">
 <head>
-<?php include(THEME_DIR_PHP.'head.php') ?>
+  <?php include(THEME_DIR_PHP.'head.php'); ?>
 </head>
 <body>
   <?php Theme::plugins('siteBodyBegin') ?>
 
   <div id="fh5co-main">
-    <div class="fh5co-intro text-center">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-md-offset-2">
-            <h1 class="intro-lead"><a class="home-title" href="<?php echo $Site->url() ?>"><?php echo $Site->title() ?></a></h1>
-            <p class=""><?php echo $Site->description() ?></p>
-          </div>
-        </div>
-      </div>
-    </div>
-
     <div id="fh5co-content">
       <div class="container">
         <div class="row">
-          <div class="col-md-10 col-md-offset-1">
-            <div class="row">
-              <div class="col-md-3">
-              <?php
-                include(THEME_DIR_PHP.'sidebar.php');
-              ?>
-              </div>
-              <div class="col-md-9">
-              <?php
-                if ($WHERE_AM_I=='page') {
-                  include(THEME_DIR_PHP.'page.php');
-                } else {
-                  include(THEME_DIR_PHP.'home.php');
-                }
-              ?>
+          <div class="col-md-3">
+            <div id="fh5co-main">
+              <div class="fh5co-intro text-center">
+                <h1 class="intro-lead"><a class="home-title" href="<?php echo $Site->url() ?>"><?php echo $Site->title() ?></a></h1>
+                <p class=""><?php echo $Site->description() ?></p>
               </div>
             </div>
+            <?php include(THEME_DIR_PHP.'sidebar.php'); ?>
+          </div>
+          <div class="col-md-9">
+            <?php
+              if ($WHERE_AM_I=='page') {
+                include(THEME_DIR_PHP.'page.php');
+              } else {
+                include(THEME_DIR_PHP.'home.php');
+              }
+            ?>
           </div>
         </div>
       </div>
     </div>
-
   </div>
 
   <footer id="fh5co-footer">
     <div class="container">
       <div class="row">
         <div class="col-md-10 col-md-offset-1 text-center">
-          <p><?php echo $Site->footer() ?><br>Powered by <a href="https://www.bludit.com" target="_blank">BLUDIT</a></p>
+          <p><?php echo $Site->footer(); ?><br>Powered by <a href="https://www.bludit.com" target="_blank">BLUDIT</a></p>
         </div>
       </div>
     </div>
   </footer>
 
-  <?php Theme::plugins('siteBodyEnd') ?>
+  <?php Theme::plugins('siteBodyEnd'); ?>
 </body>
 </html>
--- php/head.php
+++ php/head.php
@@ -1,24 +1,20 @@
 <?php
-	echo Theme::charset('utf-8');
-	echo Theme::viewport('width=device-width, initial-scale=1');
+  echo Theme::charset('utf-8');
+  echo Theme::viewport('width=device-width, initial-scale=1');
 
-	echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
-	echo '<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&amp;subset=cyrillic,greek,vietnamese" rel="stylesheet">';
+  echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
 
-	echo Theme::headTitle();
-	echo Theme::headDescription();
+  echo Theme::headTitle();
+  echo Theme::headDescription();
 
-	echo Theme::favicon('img/favicon.png');
+  echo Theme::favicon('img/favicon.png');
 
-	echo Theme::fontAwesome();
+  echo Theme::fontAwesome();
 
-        echo Theme::css('css/style.css');
-	echo Theme::css('css/bludit.css');
+  echo '<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&amp;subset=cyrillic,greek,vietnamese" rel="stylesheet">';
+  echo '<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">';
+  echo Theme::css('css/style.css');
 
-	echo '<!--[if lt IE 9]>';
-	echo Theme::js('js/respond.min.js');
-	echo '<![endif]-->';
-
-        // Load plugins with the hook siteHead
-        Theme::plugins('siteHead');
+  // Load plugins with the hook siteHead
+  Theme::plugins('siteHead');
 ?>
