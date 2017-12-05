b22fafcd255884382d3520b8490ff1000acd3f06
Marek Rost <marek.rost@gmail.com>
2017-12-05 07:55:48 +0100

    css changes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
   <div id="fh5co-main">
     <div id="fh5co-content">
-      <div class="container">
+      <div class="container-fluid">
         <div class="row">
           <div class="col-md-3">
             <div id="fh5co-main">
@@ -19,7 +19,7 @@
             </div>
             <?php include(THEME_DIR_PHP.'sidebar.php'); ?>
           </div>
-          <div class="col-md-9">
+          <div class="col-md-9 col-lg-8">
             <?php
               if ($WHERE_AM_I=='page') {
                 include(THEME_DIR_PHP.'page.php');
