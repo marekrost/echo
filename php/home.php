64eab137fabb3c9448513dd0648641bcc42b0682
Marek Rost <marek.rost@gmail.com>
2017-12-06 03:37:24 +0100

    added date boxes to home template as well

--- php/home.php
+++ php/home.php
@@ -1,30 +1,35 @@
 <!-- Section -->
 <section class="content">
-        <?php foreach ($pages as $page): ?>
-                <article class="page">
-                        <?php Theme::plugins('pageBegin') ?>
-                        <header>
-                                <a href="<?php echo $page->permalink() ?>">
-                                        <h2><?php echo $page->title() ?></h2>
-                                </a>
-
-                                <?php if( $page->coverImage() ) { ?>
-                                        <img src="<?php echo $page->coverImage() ?>" alt="<?php echo $page->slug() ?>">
-                                <?php } ?>
-                        </header>
-                        <?php echo $page->contentBreak() ?>
-                        <footer>
-                                <?php if ($page->readMore() ) { ?>
-                                <div class="readmore">
-                                        <a href="<?php echo $page->permalink() ?>">
-                                                <i class="icon-arrow-down"></i> <?php echo $Language->get('Read more') ?>
-                                        </a>
-                                </div>
-                                <?php } ?>
-                        </footer>
-                        <?php Theme::plugins('pageEnd') ?>
-                </article>
-        <?php endforeach ?>
+  <?php foreach ($pages as $page): ?>
+  <article class="page">
+    <div class="publish-date">
+      <?php $published = strtotime($page->date()); ?>
+      <div class="month"><?php echo strftime("%b", $published); ?></div>
+      <div class="day"><?php echo strftime("%d", $published); ?></div>
+      <div class="year"><?php echo strftime("%Y", $published); ?></div>
+    </div>
+    <?php Theme::plugins('pageBegin') ?>
+    <header>
+      <a href="<?php echo $page->permalink() ?>">
+        <h2><?php echo $page->title() ?></h2>
+      </a>
+      <?php if( $page->coverImage() ) { ?>
+        <img src="<?php echo $page->coverImage() ?>" alt="<?php echo $page->slug() ?>">
+      <?php } ?>
+    </header>
+    <?php echo $page->contentBreak() ?>
+    <footer>
+      <?php if ($page->readMore() ) { ?>
+        <div class="readmore">
+          <a href="<?php echo $page->permalink() ?>">
+            <i class="icon-arrow-down"></i> <?php echo $Language->get('Read more') ?>
+          </a>
+        </div>
+      <?php } ?>
+    </footer>
+    <?php Theme::plugins('pageEnd') ?>
+  </article>
+  <?php endforeach ?>
 </section>
 
 <!-- Pagination -->
