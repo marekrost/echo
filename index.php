6cd2fe0a489007b20e7cc5b80dbab444728059ba
Marek Rost <marek.rost@gmail.com>
2017-12-10 13:31:56 +0100

    added bootstrap js

--- index.php
+++ index.php
@@ -40,5 +40,7 @@
   </div>
 
   <?php Theme::plugins('siteBodyEnd'); ?>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
