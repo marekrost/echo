a3cac1e19d0d232b18d6663ec5e13f62c2179818
Marek Rost <marek.rost@gmail.com>
2019-01-06 15:42:16 +0100

    tweaked footer display of social links to integrate into other links

--- php/footer.php
+++ php/footer.php
@@ -3,23 +3,9 @@
     <div class="col-md-12">
       <p><?php echo $site->footer(); ?><br />
          <?php if ($plugins['all']['pluginRSS']->installed()):?><a href="<?php echo Theme::rssUrl(); ?>">RSS Feed</a> &#124; <?php endif ?>
-         Powered by <img class="mini-logo" src="<?php echo DOMAIN_THEME_IMG.'favicon.png'; ?>"/><a target="_blank" href="https://www.bludit.com">Bludit</a>
+         <?php foreach (Theme::socialNetworks() as $key=>$label) { ?><a href="<?php echo $site->{$key}(); ?>" rel="nofollow" target="_blank"><?php echo $label; ?></a> &#124; <?php } ?>
+         Powered by <a target="_blank" href="https://www.bludit.com">Bludit</a>
       </p>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12">
-
-      <!-- Social Networks -->
-      <?php foreach (Theme::socialNetworks() as $key=>$label): ?>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo $site->{$key}(); ?>" target="_blank">
-          <img class="d-none d-sm-block nav-svg-icon" src="<?php echo DOMAIN_THEME.'img/'.$key.'.svg' ?>" alt="<?php echo $label ?>" />
-          <span class="d-inline d-sm-none"><?php echo $label; ?></span>
-        </a>
-      </li>
-      <?php endforeach; ?>
-
     </div>
   </div>
 </footer>
